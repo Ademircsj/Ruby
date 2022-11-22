6bffb37f2883bb13c07004c3100306dc6a30f240
Ademir Colares Dos Santos Junior <ademircsj@gmail.com>
2022-11-22 11:32:24 -0300

    Questão

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,4 @@
-#Implement a caesar cipher that takes in a string and the shift #factor and then outputs the modified string:
+#Problema 1: Implement a caesar cipher that takes in a string and the shift #factor and then outputs the modified string:
 
 def ler_string ()
     puts "Informe uma string: "
