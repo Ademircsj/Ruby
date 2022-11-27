ffa44035a1378a04f7d2579529ca753079b37295
Ademir Colares Dos Santos Junior <ademircsj@gmail.com>
2022-11-27 13:31:21 -0300

    Update stock_picker.rb

--- Exercícios/stock_picker.rb
+++ Exercícios/stock_picker.rb
@@ -1,4 +1,4 @@
-#Probelama 2: Implement a method #stock_picker that takes in an array of stock prices,
+#Problem 2: Implement a method #stock_picker that takes in an array of stock prices,
 #one for each hypothetical day. It should return a pair of days representing
 #the best day to buy and the best day to sell. Days start at 0.
 
