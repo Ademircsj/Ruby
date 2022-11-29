f7d5f26c1774328920aa5417a3a551d7cb1a63bd
Ademir Colares Dos Santos Junior <ademircsj@gmail.com>
2022-11-29 00:01:58 -0300

    Update bubble_sort.rb

--- Exercicios/bubble_sort.rb
+++ Exercicios/bubble_sort.rb
@@ -1,1 +1,13 @@
 #Build a method #bubble_sort that takes an array and returns a sorted #array. It must use the bubble sort methodology (using #sort would be #pretty pointless, wouldn’t it?).
+
+
+
+def bubble_sort(vetor)
+    
+     
+end
+
+
+
+vetor = [6,3,5,8,1]
+bubble_sort(vetor)
